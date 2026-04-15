059a751f0727b8149a953324a5f6e8dc5babbe38
LohitakshReddyG <gudurulohitakshreddy07@gmail.com>
2026-04-15 23:18:04 +0530

    11 UC10 - Count Total Seats in Train (reduce)

--- APP/src/app/TrainConsistApp.java
+++ APP/src/app/TrainConsistApp.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class UseCase12TrainConsistMgmntTest {
 
     @Test
